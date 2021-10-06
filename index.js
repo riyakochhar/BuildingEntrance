b5b46312b39380ad187dbd469621241949774b81
riyakochhar <87761742+riyakochhar@users.noreply.github.com>
2021-10-06 23:50:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const path=require('path');
+const Data=require('./models/dat');
+const methodOverride=require('method-override');
+const dotenv=require('dotenv');
+
+
+dotenv.config()
+
+const connectDb=async()=>{
+    try {
+       
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+          useUnifiedTopology: true,
+          useNewUrlParser: true
+        })
+   
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+      } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+      }
+}
+
+connectDb()
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.get('/home',async (req,res)=>{
+    const data=await Data.find({});
+    res.render('home',{data});
+})
+app.get('/home/enter',(req,res)=>{
+    res.render('enter');
+})
+app.post('/home',async(req,res)=>{
+    const {name,email,phone,cinh,cinm}=req.body;
+    sendemail(email,cinh,cinm);
+    await Data.create({name,email,phone,cinh,cinm});
+    res.redirect('/home');
+})
+app.get('/home/:id',async(req,res)=>{
+    const {id}=req.params;
+    const d=await Data.findById(id);
+    res.render('exit',{d});
+})
+app.put('/home/:id',async(req,res)=>{
+    const {id}=req.params;
+    const {couth,coutm}=req.body;
+    const oh=couth;
+    const om=coutm;
+    const d=await Data.findById(id)
+    sendexmail(d.email,oh,om)
+    await Data.findByIdAndUpdate(id,{$set:{status:"Checked Out",couth:oh,coutm,om}});
+    res.redirect('/home');
+})
+app.delete('/home/:id',async (req,res)=>{
+    const {id}=req.params;
+    await Data.findByIdAndDelete(id);
+    res.redirect('/home');
+})
+
+
+function sendemail(email,cinh,cinm){
+    const sgMail=require('@sendgrid/mail');
+   const  sendgrid=process.env.SG_KEY
+   //kochhar api key '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';
+   //kochharFull(access) api key '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';
+  sgMail.setApiKey(sendgrid);
+  let m=cinm.toString();
+  let h=cinh.toString();;
+  if(cinm<=9){
+      m='0'+cinm.toString();
+  }
+  if(cinh<=9){
+      h='0'+cinh.toString();
+  }
+  const msg={
+      to: email,
+      from: process.env.MAIL,
+      subject:"Entering building",
+      text:`Hi you entered the building at ${h}:${m}`
+     //text:'Hi from sendgrid'
+  };
+  sgMail.send(msg);
+}
+
+
+function sendexmail(email,couth,coutm){
+    const sgMail=require('@sendgrid/mail');
+   const  sendgrid=process.env.SG_KEY
+  sgMail.setApiKey(sendgrid);
+  let m=coutm.toString();
+  let h=couth.toString();
+  if(coutm<=9){
+      m='0'+coutm.toString();
+  }
+  if(couth<=9){
+      h='0'+couth.toString()
+  }
+  const msg={
+      to: email, 
+      from: process.env.MAIL,
+      subject:"Checking out",
+      text:`Hi you checked out at ${h}:${m}`
+    //text:'hi bye'
+  };
+  sgMail.send(msg);
+}
+
+app.listen(process.env.PORT,(req,res)=>{
+    console.log("UP AT 3000");
+})
